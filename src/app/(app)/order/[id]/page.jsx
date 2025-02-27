1e190cd0f2bfe61322cee84e144e4a5208fc77f1
fendnugraha <fendnugraha92@gmail.com>
2025-02-27 19:30:28 +0700

    menuju order detail

--- src/app/(app)/order/[id]/page.jsx
+++ src/app/(app)/order/[id]/page.jsx
@@ -2,16 +2,17 @@
 import { use, useEffect, useState } from "react";
 import Header from "../../Header";
 import axios from "@/libs/axios";
+import Link from "next/link";
+import { ArrowLeftIcon } from "lucide-react";
 
 const OrderDetail = ({ params }) => {
     const { id } = use(params);
-    console.log(id);
 
-    const [order, setOrder] = useState([{}]);
+    const [order, setOrder] = useState({});
     const fetchOrderById = async (id) => {
         try {
             const response = await axios.get(`/api/orders/${id}`);
-            setOrder(response.data.data);
+            setOrder(response.data.order);
         } catch (error) {
             setErrors(error.response?.data?.errors || ["Something went wrong."]);
         }
@@ -20,19 +21,56 @@ const OrderDetail = ({ params }) => {
     useEffect(() => {
         fetchOrderById(id);
     }, [id]);
-    console.log(order);
     return (
         <>
             <Header title="Order Detail" />
             <div className="py-8 relative">
                 <div className="max-w-7xl mx-auto sm:px-6">
+                    <div className="flex justify-between">
+                        <h1 className="text-2xl font-bold mb-4">{order?.order_number}</h1>
+                        <Link href="/order" className="text-sm font-semibold hover:underline">
+                            <ArrowLeftIcon className="w-4 h-4 inline mr-2" /> Kembali
+                        </Link>
+                    </div>
                     <div className="grid grid-cols-2 gap-2">
-                        <div className="bg-white rounded-2xl p-4">
-                            <table className="w-full tex-xs">
+                        <div className="bg-white rounded-2xl p-4 shadow-lg border border-slate-300">
+                            <table className="w-full">
                                 <tbody>
                                     <tr>
-                                        <td>Nama Konsumen</td>
-                                        <td>Fend</td>
+                                        <td className="text-sm font-bold py-1">Nama Konsumen</td>
+                                        <td className="text-sm text-right">{order?.contact?.name}</td>
+                                    </tr>
+                                    <tr>
+                                        <td className="text-sm font-bold py-1">No. Handphone</td>
+                                        <td className="text-sm text-right">{order?.contact?.phone_number}</td>
+                                    </tr>
+                                    <tr>
+                                        <td className="text-sm font-bold py-1">Alamat</td>
+                                        <td className="text-sm text-right"></td>
+                                    </tr>
+                                    <tr>
+                                        <td colSpan={2} className="text-sm">
+                                            {order?.contact?.address}
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <div className="bg-white rounded-2xl p-4 shadow-lg border border-slate-300">
+                            <table className="w-full">
+                                <tbody>
+                                    <tr>
+                                        <td className="text-sm font-bold py-1">Type HP</td>
+                                        <td className="text-sm text-right">{order?.phone_type}</td>
+                                    </tr>
+                                    <tr>
+                                        <td className="text-sm font-bold py-1">Deskripsi Kerusakan</td>
+                                        <td className="text-sm text-right"></td>
+                                    </tr>
+                                    <tr>
+                                        <td colSpan={2} className="text-sm bg-slate-300 px-4 py-2">
+                                            {order?.description}
+                                        </td>
                                     </tr>
                                 </tbody>
                             </table>
