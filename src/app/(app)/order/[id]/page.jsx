c44198dc7dfd538ba53ff95a745fb26f4c642478
fendnugraha <fendnugraha92@gmail.com>
2025-03-01 22:43:48 +0700

    design order detail add parts

--- src/app/(app)/order/[id]/page.jsx
+++ src/app/(app)/order/[id]/page.jsx
@@ -3,10 +3,15 @@ import { use, useEffect, useState } from "react";
 import Header from "../../Header";
 import axios from "@/libs/axios";
 import Link from "next/link";
-import { ArrowLeftIcon, MinusCircleIcon, PlusCircleIcon, SmartphoneIcon, XCircleIcon } from "lucide-react";
+import { ArrowLeftIcon, EditIcon, MinusCircleIcon, PlusCircleIcon, SmartphoneIcon, XCircleIcon } from "lucide-react";
+import Modal from "@/components/Modal";
+import Input from "@/components/Input";
 
 const OrderDetail = ({ params }) => {
     const { id } = use(params);
+    const [errors, setErrors] = useState([]);
+    const [isModalAddPartOpen, setIsModalAddPartOpen] = useState(false);
+    const closeModal = () => setIsModalAddPartOpen(false);
 
     const formatPhoneNumber = (phone) => {
         if (!phone) return ""; // Handle jika input null/undefined
@@ -52,20 +57,66 @@ const OrderDetail = ({ params }) => {
                                 </div>
                             </div>
                             <div className="mb-2 grid grid-cols-2 gap-2">
-                                <button className="bg-indigo-500 hover:bg-indigo-400 text-white rounded-lg text-sm p-2">
+                                <button
+                                    onClick={() => setIsModalAddPartOpen(true)}
+                                    className="bg-indigo-500 hover:bg-indigo-400 text-white rounded-lg text-sm p-2"
+                                >
                                     <PlusCircleIcon size={16} className="inline" /> Add Parts
                                 </button>
+                                <Modal isOpen={isModalAddPartOpen} onClose={closeModal} modalTitle="Add Parts Replacement" maxWidth="max-w-md">
+                                    <Input type="search" className={"w-full rounded-2xl"} placeholder="Cari barang / parts" />
+                                    <div className="px-4 py-2 border-b border-slate-200 border-dashed">
+                                        <div className="flex justify-between itemts-center mb-2">
+                                            <h1 className="text-sm text-slate-700 text-nowrap overflow-hidden">LCD Iphone 13 Premium</h1>
+                                        </div>
+                                        <div className="flex justify-between">
+                                            <div className="flex gap-4 items-center">
+                                                <button>
+                                                    <MinusCircleIcon size={16} />
+                                                </button>
+                                                <h1 className="text-sm ">1</h1>
+                                                <button>
+                                                    <PlusCircleIcon size={16} />
+                                                </button>
+                                            </div>
+                                            <input type="number" className="w-1/2 border border-slate-300 rounded-md p-1 text-xs text-end" />
+                                        </div>
+                                        <div className="flex justify-between items-start">
+                                            <button className="bg-blue-500 hover:bg-blue-400 text-white rounded-md text-xs p-2 mt-2">
+                                                Tambah ke Cart <PlusCircleIcon size={16} className="inline" />
+                                            </button>
+                                            <h1 className="text-xs text-slate-500 mt-2">Subtotal: Rp. 1.000.000</h1>
+                                        </div>
+                                    </div>
+                                    <div className="px-4 py-2 border-b border-slate-200 border-dashed">
+                                        <div className="flex justify-between itemts-center mb-2">
+                                            <h1 className="text-sm text-slate-700 text-nowrap overflow-hidden">LCD Iphone 13 Premium</h1>
+                                        </div>
+                                        <div className="flex justify-between">
+                                            <div className="flex gap-4 items-center">
+                                                <button>
+                                                    <MinusCircleIcon size={16} />
+                                                </button>
+                                                <h1 className="text-sm ">1</h1>
+                                                <button>
+                                                    <PlusCircleIcon size={16} />
+                                                </button>
+                                            </div>
+                                            <input type="number" className="w-1/2 border border-slate-300 rounded-md p-1 text-xs text-end" />
+                                        </div>
+                                        <div className="flex justify-between items-start">
+                                            <button className="bg-blue-500 hover:bg-blue-400 text-white rounded-md text-xs p-2 mt-2">
+                                                Tambah ke Cart <PlusCircleIcon size={16} className="inline" />
+                                            </button>
+                                            <h1 className="text-xs text-slate-500 mt-2">Subtotal: Rp. 1.000.000</h1>
+                                        </div>
+                                    </div>
+                                </Modal>
                                 <button className="bg-red-500 hover:bg-red-400 text-white rounded-lg text-sm p-2">
                                     <PlusCircleIcon size={16} className="inline" /> Biaya Lainnya
                                 </button>
                             </div>
-                            <div className="bg-white rounded-2xl px-4 py-2 mb-2 shadow-sm border border-slate-300">
-                                <h1 className="text-sm text-slate-500">Total Price</h1>
-                                <h1 className="text-2xl font-bold">
-                                    <sup>Rp</sup> 5.000.000
-                                </h1>
-                            </div>
-                            <div className="bg-white rounded-2xl px-4 py-2 shadow-sm border border-slate-300">
+                            <div className="px-4 py-2 bg-white rounded-2xl">
                                 <div className="mb-1">
                                     <h1 className="text-sm text-slate-500">Nama Konsumen</h1>
                                     <h1 className="text-sm font-bold">{order?.contact?.name}</h1>
@@ -76,18 +127,24 @@ const OrderDetail = ({ params }) => {
                                 </div>
                                 <div className="mb-1">
                                     <h1 className="text-sm text-slate-500">Deskripsi Kerusakan</h1>
-                                    <h1 className="text-sm p-2 bg-slate-300 rounded-xl">{order?.description}</h1>
+                                    <h1 className="text-xs p-2 bg-slate-300 rounded-xl">{order?.description}</h1>
                                 </div>
                             </div>
                         </div>
                         {/* Part Replacement Table */}
-                        <div className="flex flex-col gap-1">
-                            <div className="bg-white rounded-2xl px-4 py-2 shadow-sm border border-slate-300">
+                        <div className="flex flex-col gap-1 bg-white rounded-2xl shadow-sm col-span-2">
+                            <h1 className="text-left text-sm font-semibold text-slate-500 px-4 pt-2 mb-2">Part Replacement</h1>
+                            <div className="px-4 py-2 border-b border-slate-200 border-dashed">
                                 <div className="flex justify-between itemts-center mb-2">
                                     <h1 className="text-sm text-slate-700 text-nowrap overflow-hidden">LCD Iphone 13 Premium</h1>
-                                    <button>
-                                        <XCircleIcon size={16} className="text-red-500" />
-                                    </button>
+                                    <div className="">
+                                        <button className="mr-1">
+                                            <EditIcon size={16} className="text-green-500" />
+                                        </button>
+                                        <button>
+                                            <XCircleIcon size={16} className="text-red-500" />
+                                        </button>
+                                    </div>
                                 </div>
                                 <div className="flex justify-between">
                                     <div className="flex gap-4 items-center">
@@ -103,12 +160,17 @@ const OrderDetail = ({ params }) => {
                                 </div>
                                 <h1 className="text-xs text-right text-slate-500 mt-2">Subtotal: Rp. 1.000.000</h1>
                             </div>
-                            <div className="bg-white rounded-2xl px-4 py-2 shadow-sm border border-slate-300">
+                            <div className="px-4 py-2 border-b border-slate-200 border-dashed">
                                 <div className="flex justify-between itemts-center mb-2">
                                     <h1 className="text-sm text-slate-700 text-nowrap overflow-hidden">LCD Iphone 13 Premium</h1>
-                                    <button>
-                                        <XCircleIcon size={16} className="text-red-500" />
-                                    </button>
+                                    <div className="">
+                                        <button className="mr-1">
+                                            <EditIcon size={16} className="text-green-500" />
+                                        </button>
+                                        <button>
+                                            <XCircleIcon size={16} className="text-red-500" />
+                                        </button>
+                                    </div>
                                 </div>
                                 <div className="flex justify-between">
                                     <div className="flex gap-4 items-center">
@@ -124,12 +186,17 @@ const OrderDetail = ({ params }) => {
                                 </div>
                                 <h1 className="text-xs text-right text-slate-500 mt-2">Subtotal: Rp. 1.000.000</h1>
                             </div>
-                            <div className="bg-white rounded-2xl px-4 py-2 shadow-sm border border-slate-300">
+                            <div className="px-4 py-2 border-b border-slate-200 border-dashed">
                                 <div className="flex justify-between itemts-center mb-2">
                                     <h1 className="text-sm text-slate-700 text-nowrap overflow-hidden">LCD Iphone 13 Premium</h1>
-                                    <button>
-                                        <XCircleIcon size={16} className="text-red-500" />
-                                    </button>
+                                    <div className="">
+                                        <button className="mr-1">
+                                            <EditIcon size={16} className="text-green-500" />
+                                        </button>
+                                        <button>
+                                            <XCircleIcon size={16} className="text-red-500" />
+                                        </button>
+                                    </div>
                                 </div>
                                 <div className="flex justify-between">
                                     <div className="flex gap-4 items-center">
@@ -144,9 +211,20 @@ const OrderDetail = ({ params }) => {
                                     <h1 className="text-sm font-bold">Rp. 1.000.000</h1>
                                 </div>
                                 <h1 className="text-xs text-right text-slate-500 mt-2">Subtotal: Rp. 1.000.000</h1>
+                            </div>
+                            <h1 className="text-xs text-right text-slate-500 px-4">3 items</h1>
+                            <div className="text-right px-4 pb-2">
+                                <h1 className="text-xs text-slate-500">
+                                    Biaya Jasa Teknisi (Service fees): <span className="block text-sm font-bold">Rp. 1.000.000</span>
+                                </h1>
+                                <h1 className="text-xs text-slate-500">
+                                    Biaya Pengiriman (Shipping): <span className="block text-sm font-bold">Rp. 1.000.000</span>
+                                </h1>
+                                <h1 className="text-xs text-blue-500">
+                                    Total: <span className="block text-2xl font-bold">Rp. 5.000.000</span>
+                                </h1>
                             </div>
                         </div>
-                        <div className="bg-white rounded-2xl px-4 py-2 shadow-sm border border-slate-300"></div>
                     </div>
                 </div>
             </div>
